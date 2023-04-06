c42260f60f56f5f1d701cd4539f5ee0d557b9b27
Sultan <sultanfastakhiev@gmail.com>
2023-04-06 14:12:29 +0300

    first commit

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,62 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
+ 
+ <div class="example-2">
+   <div class="form-group">
+     <input  @change="onFileSelected" type="file" name="file" id="file" class="input-file">
+     <label for="file" class="btn btn-tertiary js-labelFile">
+       <i class="icon fa fa-check"></i>
+       <span class="js-fileName">Загрузить файл</span>
+     </label>
+   </div>
+   <p>{{ this.benz_92 }}</p>
+   <p>{{ this.benz_95 }}</p>
+   <p>{{ this.benz_98 }}</p>
   </div>
 </template>
 
 <script>
+import axios from 'axios'
 export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
+  data () {
+    return {
+      selectedFile: null,
+      benz_92: null,
+      benz_95: null,
+      benz_98: null,
+    }
+  },
+  methods: {
+    onFileSelected(event) {
+      this.selectedFile = event.target.files[0]
+      // console.log(event)
+      // var user = {
+      //   user: "Fred"
+      // }
+      const config = {
+        headers: {
+          'content-type': 'multipart/form-data'
+        }
+      }
+      const file = new FormData();
+      file.append('file', this.selectedFile)
+      axios.post('http://127.0.0.1:8000/upload_file', file, config).then(res=>{
+        console.log(res.data)
+        this.benz_92 = "92 бензин" + " " + res.data.benz_92 + "%"
+        this.benz_95 = "95 бензин" + " " + res.data.benz_95 + "%"
+        this.benz_98 = "98 бензин" + " " + res.data.benz_98 + "%"
+      })
+    }
   }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
+.example-2 .btn-tertiary{color:#555;padding:0;line-height:400px;width:800px;margin:auto;display:block;border:2px solid #555}
+.example-2 .btn-tertiary:hover,.example-2 .btn-tertiary:focus{color:#888;border-color:#888}
+.example-2 .input-file{width:.1px;height:.1px;opacity:0;overflow:hidden;position:absolute;z-index:-1}
+.example-2 .input-file + .js-labelFile{overflow:hidden;text-overflow:ellipsis;white-space:nowrap;padding:0 10px;cursor:pointer}
+.example-2 .input-file + .js-labelFile .icon:before{content:"\f093"}
+.example-2 .input-file + .js-labelFile.has-file .icon:before{content:"\f00c";color:#5AAC7B}
 </style>
